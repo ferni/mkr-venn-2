e484b85001abc31b6246d349df3babc0fdf69423
Saabi <ushiferreyra@gmail.com>
2018-03-02 23:47:54 -0300

    Added realtime redraw.

--- src/main.js
+++ src/main.js
@@ -8,25 +8,32 @@ import paper from "paper";
 validateData(data);
 const canvas = document.getElementById('canvas');
 paper.setup(canvas);
+window.paper = paper;
 
 // Activate a temp draw layer only for processing
 const tempLayer = new paper.Layer();
 let model = getModel(data);
-model = optimizeScore(data, model);
+model = optimizeScore(data, model, draw);
 const score = getScore(data, model);
 console.log('Final score is ' + score);
 
-// Remove temp layer and activate actual draw layer
-tempLayer.remove();
-new paper.Layer();
 
-// Remake items for drawing in new layer
-model.labels.forEach(label => {
-  label.paperItem = null;
-  label.updatePaperItem();
-});
-model.circles.forEach(circle => {
-  const shape = new paper.Shape.Circle(new paper.Point(circle.x, circle.y), circle.r);
-  shape.strokeColor = 'black';
-});
-paper.view.draw();
+function draw(model) {
+  // Remove temp layer and activate actual draw layer
+  //paper.project.activeLayer.remove();
+  //new paper.Layer();
+  paper.project.activeLayer.clear();
+  // Remake items for drawing in new layer
+  model.labels.forEach(label => {
+    label.paperItem = null;
+    label.updatePaperItem();
+  });
+  model.circles.forEach(circle => {
+    const shape = new paper.Shape.Circle(new paper.Point(circle.x, circle.y), circle.r);
+    shape.strokeColor = 'black';
+  });
+  //canvas.msGetInputContext('2d');
+  //canvas.clearReact
+  paper.view.draw();
+}
+draw(model);
