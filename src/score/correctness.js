d48be7bab8a17ed0c466500894b6641680b4541b
Fernando Tiberti <fertiberti@gmail.com>
2018-03-08 21:52:15 -0300

    Judge labels being inside circles by all its vertices instead of just the position.

--- src/score/correctness.js
+++ src/score/correctness.js
@@ -1,17 +1,21 @@
-import {circleContainsPoint} from "../geometry";
+import {circleContainsPoint, getDistance} from "../geometry";
 
 // Gives score based on if the members are in and only in their respective groups
 function getCorrectnessScore(data, model) {
   let score = 0;
   model.labels.forEach(label => {
     model.circles.forEach(circle => {
-      if (circleContainsPoint(circle, label)) {
-        if (circle.labels.some(l => l === label)) {
-          score += 1;
-        } else {
-          score -= 1;
+      label.getVertices().forEach(point => {
+        if (circleContainsPoint(circle, point)) {
+          if (circle.labels.some(l => l === label)) {
+            score += 1;
+          } else {
+            // substract proportionally to how much is the point
+            // inside a circle it's not supposed to
+            score -= ((circle.r - getDistance(circle, point)) / 500); // todo: get the 500 value from actual canvas width
+          }
         }
-      }
+      })
     })
   });
   return score;
